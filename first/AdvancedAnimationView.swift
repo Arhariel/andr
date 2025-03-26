a4b5fc8c9e7957c21c90603d49cbe38693644a76
Arhariel <arharie@yandex.ru>
2025-03-26 17:19:25 +0500

    all changes

---
+++ first/AdvancedAnimationView.swift
@@ -0,0 +1,8 @@
+//
+//  AdvancedAnimationView.swift
+//  first
+//
+//  Created by IGOR FEL on 12.03.2025.
+//
+
+import Foundation
---
+++ first/SearchListView.swift
@@ -0,0 +1,8 @@
+//
+//  SearchListView.swift
+//  first
+//
+//  Created by IGOR FEL on 12.03.2025.
+//
+
+import Foundation
